0539ae054d07fd13e51a163d9faa500e22ed7e95
ulises <ulisesdejesusk9999@hotmail.com>
2019-02-02 23:55:53 -0600

    iniciemos

---
+++ primer_programa_en_C.cpp
@@ -0,0 +1,19 @@
+/* Programa: Hola mundo */
+
+#include <conio.h>
+#include <stdio.h>
+
+int main()
+{
+
+
+
+    printf( "Hola mundo." );
+   
+
+ 
+    getch(); /* Pausa */
+
+    return 0;
+} resultado=pow(a+b,3)/c;
+  		  printf("resultado %f",resultado);
