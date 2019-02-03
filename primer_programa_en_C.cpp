35d00979c2cd58ed903e6fd1d87590ab121dd0dc
ulises <ulisesdejesusk9999@hotmail.com>
2019-02-03 09:05:59 -0600

    pedimos el valor de B

--- primer_programa_en_C.cpp
+++ primer_programa_en_C.cpp
@@ -6,14 +6,15 @@
 int main()
 {
 
-int a;
+int a,b;
 
    printf( " Hola mundo \n" );
    printf( " este es mi primer programa en c\n" );
    printf( " la suma de dos numeros A y B\n" );
     printf( "dame el primer valor para A = " );//primera rama prueva 1
     scanf("%d", &a);
- 
+     printf( "dame el segundo valor para B = " );//primera rama prueva 1
+     scanf("%d", &b);
     getch(); /* Pausa */
 
     return 0;
